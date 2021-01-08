a266d9ddbb09ea1f4f34ef552853004aad29728b
Kurt Harriger <kharriger@atlassian.com>
2021-01-08 15:56:35 -0700

    working dev build

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,11 +1,14 @@
 (ns app.core
   (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
             [app.views :refer [app]]))
+
 
 (defn ^:dev/after-load start []
   (js/console.log "Starting...")
-  (r/render [app]
+  (rdom/render [app]
             (.getElementById js/document "root")))
+
 
 (defn ^:export main []
   (start))
